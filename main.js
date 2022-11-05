e389951aacd4655c5a9f9faf8bbe532cfb202348
Nathan Stanford <Nathan.Stanford@gmail.com>
2022-11-05 11:07:03 -0400

    Update format of javascript

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-
-function AddDiv() {
+// Add Div from hidden Div
+function AddDiv()
+{
   // copy hidden div change name and add to main div
   var div0 = document.getElementById('div0');
   var div0Clone = div0.cloneNode(true);
-  div0Clone.id =
-    'div' + (document.getElementById('main').childElementCount + 1);
+  div0Clone.id = 'div' + (document.getElementById('main').childElementCount + 1);
   div0Clone.style.display = 'block';
   // rename input fields
   var inputs = div0Clone.getElementsByTagName('input');
@@ -12,7 +12,6 @@ function AddDiv() {
     inputs[i].id = inputs[i].id + (document.getElementById('main').childElementCount + 1);
     inputs[i].name = inputs[i].name + (document.getElementById('main').childElementCount + 1);
   }
-  
   // rename select fields
   var selects = div0Clone.getElementsByTagName('select');
   for (var i = 0; i < selects.length; i++) {
@@ -21,16 +20,17 @@ function AddDiv() {
   }
   document.getElementById('main').appendChild(div0Clone);
 }
-
+// Delete the div
 function deleteDiv(btn) {
   var div = btn.parentNode.parentNode.parentNode;
   div.parentNode.removeChild(div);
 }
-
+// Update the Amount
 function updateAmount() {
   var totalAmount = 0;
-  // loop through all inputs starts with amount and get amount
+  // get all the inputs
   var inputs = document.getElementsByTagName('input');
+  // loop through all inputs starts with amount and get amount
   for (var i = 0; i < inputs.length; i++) {
     if (inputs[i].id.startsWith('amount_')) {
       var amount = inputs[i].value;
@@ -42,6 +42,6 @@ function updateAmount() {
       }
     }
   }
-  
+  // update total amount
   document.getElementById("totalAmount").innerHTML = totalAmount;
 }
